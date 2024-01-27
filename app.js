930a818197bae831c660ec584fe039973470bb91
nitin-maul <nitin.maul@springernature.com>
2024-01-27 21:22:25 +0530

    Using parcel as build tool

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", {}, "Hello world from React!!!!");
 
 const nextedHeading = React.createElement("div", { id: parent }, [
