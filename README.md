c7ac8b0e4605367d3c55f41c3227ead7d706b96c
nitin-maul <nitin.maul@springernature.com>
2024-04-11 12:14:26 +0530

    Commit#1 Understand the react with createElement, createRoot and render method

---
+++ README.md
@@ -0,0 +1,2 @@
+#Namaste React
+##Commit#1: Understand the react with createElement, createRoot and render method
--- app.js
+++ app.js
@@ -2,24 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {}, "Hello world from React!!!!");
+console.log('heading', heading);
 
-const nextedHeading = React.createElement("div", { id: parent }, [
-  React.createElement(
-    "div",
-    { id: "child1" },
-    React.createElement("h1", { id: "child1" }, "This is heading 1 inside child1")
-  ),
-  React.createElement(
-    "div",
-    { id: "child2" },
-    React.createElement("h1", { id: "child1" }, "This is heading 2 inside child2")
-  ),
-]);
-
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(heading);
-
-console.log("Heading", heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log("root", root);
-root.render(nextedHeading);
+root.render(heading);
